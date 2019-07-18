7f52de8dc975bd2f555e8a0c9a83b846775a0f0b
zhengzhiwei <714883190@qq.com>
2019-07-18 17:15:44 +0800

    zzw

---
+++ 郑志伟/插件/Oracle_DataChanged/Oracle_DataChanged/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+using System.Data;
+
+namespace Oracle_DataChanged
+{
+    public class Class1
+    {
+        private String Source;
+        private String UserID;
+        private String Password;
+
+        public Class1()
+        {
+
+            //数据源字符串其中192.168.1.21是你oracle数据库的机器ip,port是你oracle数据库的端口号,home2是你oracle数据库的服务名
+            Source = "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=192.168.0.10)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCL)))";
+            UserID = "DUPM123";           //用户名字符串
+            Password = "dupm123";  //密码
+        }
+
+        public OracleConnection GetConn()
+        {
+            OracleConnection OracleConn = new OracleConnection("Data Source=" + Source + "; User Id=" + UserID + ";Password=" + Password + ";Integrated Security=no;");
+            OracleConn.Open();
+            return OracleConn;
+        }
+    }
+}
---
+++ 郑志伟/插件/orcl_in/orcl_in/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orcl_in
+{
+    public class Class1
+    {
+    }
+}
